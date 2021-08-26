21f93178efdd7ad3297a1e613f0046527644be76
den <anirfid@mail.ru>
2021-08-26 13:34:25 +0400

    26.08.21 13:33 вернул на QueryBilder

--- app/Classes/DataDb.php
+++ app/Classes/DataDb.php
@@ -8,22 +8,22 @@ use App\Plan;
 class DataDb {
     
     public static function delEntry($id) {
-        $STH=Entry::where('id', '=', $id)->delete();
-        //$STH = DB::table('list_entry')->where('id', '=', $id)->delete(); 
+        //$STH=Entry::where('id', '=', $id)->delete();
+        $STH = DB::table('list_entry')->where('id', '=', $id)->delete(); 
         //$STH->execute($data);
     }
     
     public static function insertEntry($day,$id,$newtext) {
         $date=date('Y-m-d h:i:s');
-        $STH=Entry::insert(['day' => $day, 'id_entry' => $id, 'entry' => $newtext,'created_at'=>$date,'updated_at'=>$date]); 
-        //$STH = DB::table('list_entry')->insert(['day' => $day, 'id_entry' => $id, 'entry' => $newtext]); 
+        //$STH=Entry::insert(['day' => $day, 'id_entry' => $id, 'entry' => $newtext,'created_at'=>$date,'updated_at'=>$date]); 
+        $STH = DB::table('list_entry')->insert(['day' => $day, 'id_entry' => $id, 'entry' => $newtext]); 
         //$STH->execute($data);
     }
 
     public static function readEntry($day) {
         //global $conn;
-        $STH=Entry::where('day', '=', $day)->get();
-        //$STH = DB::table('list_entry')->where('day', '=', $day)->get();
+        //$STH=Entry::where('day', '=', $day)->get();
+        $STH = DB::table('list_entry')->where('day', '=', $day)->get();
         
         return $STH;
     }
@@ -31,23 +31,23 @@ class DataDb {
     public static function markEntry($id,$mark) {
         //global $conn;
         //$data = array('id' => $id, 'mark' => $mark ); 
-        $STH=Entry::where('id', $id)->update(['mark' => $mark]);
-        //$STH = DB::table('list_entry')->where('id', $id)->update(['mark' => $mark]);
+        //$STH=Entry::where('id', $id)->update(['mark' => $mark]);
+        $STH = DB::table('list_entry')->where('id', $id)->update(['mark' => $mark]);
         //$STH->execute($data);
     }
     
     public static function commEntry($id,$comm) {
         //global $conn;
         //$data = array('id' => $id, 'mark' => $mark ); 
-        $STH=Entry::where('id', $id)->update(['comment' => $comm]);
-        //$STH = DB::table('list_entry')->where('id', $id)->update(['comment' => $comm]);
+        //$STH=Entry::where('id', $id)->update(['comment' => $comm]);
+        $STH = DB::table('list_entry')->where('id', $id)->update(['comment' => $comm]);
         //$STH->execute($data);
     }
     
     public static function readPlans() {
         //global $conn;
-        $STH=Plan::get();
-        //$STH = DB::table('plans')->get();
+        //$STH=Plan::get();
+        $STH = DB::table('plans')->get();
         /*$STH->execute(array($day));
         $arrEntrys=$STH->fetchAll(PDO::FETCH_ASSOC);*/
         return $STH;
@@ -56,8 +56,8 @@ class DataDb {
     public static function deletePlans() {
         //global $conn;
         //$data = array('day' => $day, 'id_entry' => $id, 'entry' => $newtext ); 
-        $STH=Plan::delete();
-        //$STH = DB::table('plans')->delete();
+        //$STH=Plan::delete();
+        $STH = DB::table('plans')->delete();
         //$STH->execute($data);
     }
     
@@ -65,8 +65,8 @@ class DataDb {
         //global $conn;
         //$data = array('day' => $day, 'id_entry' => $id, 'entry' => $newtext ); 
         $date=date('Y-m-d h:i:s');
-        $STH=Plan::insert(['text' => $newtext,'created_at'=>$date,'updated_at'=>$date]);  
-       // $STH = DB::table('plans')->insert(['text' => $newtext,'created_at'=>$date,'updated_at'=>$date]);  
+        //$STH=Plan::insert(['text' => $newtext,'created_at'=>$date,'updated_at'=>$date]);  
+        $STH = DB::table('plans')->insert(['text' => $newtext,'created_at'=>$date,'updated_at'=>$date]);  
         //$STH->execute($data);
     }
     
